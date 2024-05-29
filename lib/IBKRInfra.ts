f2d99a39741f72d1d3cc5231d49521d248f836b1
RYAN LEE <ryan_lee@live.com>
2024-05-28 22:29:27 -0600

    Fixed use of deprecated cdk code

--- lib/IBKRInfra.ts
+++ lib/IBKRInfra.ts
@@ -3,25 +3,47 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudtrail from 'aws-cdk-lib/aws-cloudtrail';
-
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IBKRInfra extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        //create vpc
-        const vpc = new ec2.Vpc(this, 'MyVPC', {
-            cidr: '10.0.0.0/16'
+        //NACLs
+        const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+            natGateways: 1,
+            maxAzs: 3,
+            subnetConfiguration: [
+                {
+                    name: 'private-subnet-1',
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidrMask: 24,
+                },
+                {
+                    name: 'public-subnet-1',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24,
+                },
+                {
+                    name: 'isolated-subnet-1',
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidrMask: 28,
+                },
+            ],
         });
 
+        //create vpc
+        /* const vpc = new ec2.Vpc(this, 'MyVPC', {
+            cidr: '10.0.0.0/16'
+        }); */
+
         //create subnet
-        const subnet = new ec2.Subnet(this, 'MySubnet', {
+        /* const subnet = new ec2.Subnet(this, 'MySubnet', {
             vpcId: vpc.vpcId,
             cidrBlock: '10.0.1.0/24',
             availabilityZone: 'us-west-2a'
-        });
+        }); */
 
         //create Security Groups
         const sg = new ec2.SecurityGroup(this, 'MySecurityGroup', {
